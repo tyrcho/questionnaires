e3fce457dfffc0885907ec550ee86dee05c911d9
Michel Daviot <tyrcho@tyrcho.com>
2011-08-30 23:38:44 +0200

    display all questions + new test

--- src/com/tyrcho/scala/question/view/RunQuestionnaireView.scala
+++ src/com/tyrcho/scala/question/view/RunQuestionnaireView.scala
@@ -1,11 +1,20 @@
 package com.tyrcho.scala.question.view
 
 import scala.collection.mutable.ListBuffer
+import com.tyrcho.scala.question.model.Question
+import com.tyrcho.scala.question.model.Questionnaire
 
-trait RunQuestionnaireView {
+trait RunQuestionnaireView[Q, A] {
   val listeners = ListBuffer[() => Unit]()
 
-  def start()
+  def start(q: Questionnaire[Q, A]) {
+    q.questions foreach (displayQuestion(_))
+    fireQuestionnaireFinished
+  }
+
+  def displayQuestion(q: Question[Q, A])
+
   def registerQuestionnaireFinishedListener(listener: () => Unit) { listeners.append(listener) }
+
   protected def fireQuestionnaireFinished() { listeners foreach (_.apply()) }
 }
--- test/test/com/tyrcho/scala/question/view/TestRunQestionnaireView.scala
+++ test/test/com/tyrcho/scala/question/view/TestRunQestionnaireView.scala
@@ -3,16 +3,38 @@ package test.com.tyrcho.scala.question.view
 import org.junit.Test
 import com.tyrcho.scala.question.view.RunQuestionnaireView
 import org.junit.Assert._
+import com.tyrcho.scala.question.model.Questionnaire
+import com.tyrcho.scala.question.model.Question
+import org.junit.Before
+import scala.collection.mutable.ListBuffer
 
 class TestRunQestionnaireView {
+  var view: RunQuestionnaireView[String, String] = null
+  var answers = ListBuffer[String]()
+
+  @Before
+  def setup() {
+    view = new RunQuestionnaireView[String, String]() {
+      def displayQuestion(q: Question[String, String]) {
+        println(q.question)
+        answers.append(q.answer)
+      }
+    }
+    answers.clear
+  }
+
   @Test
   def testListener() {
-    val view = new RunQuestionnaireView() {
-      def start() { fireQuestionnaireFinished() }
-    }
     var done = false
     view.registerQuestionnaireFinishedListener(() => { done = true })
-    view.start
+    view.start(new Questionnaire[String, String](Seq(new Question("q", "a"))))
     assertTrue("listener called", done)
+  }
+
+  @Test
+  def testPrinter() {
+    val questions = Seq(new Question("q1", "a1"), new Question("q2", "a2"))
+    view.start(new Questionnaire[String, String](questions))
+    assertEquals(Seq("a1", "a2"), answers.toSeq)
   }
 }
